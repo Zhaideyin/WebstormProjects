58b8eea3cbb02d4f5cdbf5363cda07d0dd5caf79
Zhaideyin <Zhaideyin@xsscd.com>
2017-03-17 14:26:50 +0800

    Initial commit

---
+++ js/const.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tongbu on 2017/3/16 0016.
+ */
+
+var SERVER_URL="http://10.190.3.5:8080";
+function formatDate(ns){
+    var unixTimestamp = new Date(ns) ;
+    return  unixTimestamp.getFullYear() + "." + (unixTimestamp.getMonth() + 1) + "." + unixTimestamp.getDate() + "" ;
+}
